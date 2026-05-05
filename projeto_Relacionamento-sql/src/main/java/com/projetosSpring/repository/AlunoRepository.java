924f70ef35cffd5945491b96249beddf2d97acd4
Here's Jhonny! <jhonnythomazz@gmail.com>
2026-05-05 09:57:48 -0300

    Delete projeto_Relacionamento-sql directory

--- projeto_Relacionamento-sql/src/main/java/com/projetosSpring/repository/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetosSpring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetosSpring.entity.Aluno;
-
-public interface AlunoRepository extends JpaRepository <Aluno, Long>{
-
-}
